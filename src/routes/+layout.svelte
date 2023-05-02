4e5c289c2ca2ed20440287d29d740472ef085b63
Nick <nconkl1@outlook.com>
2023-05-02 16:19:51 -0400

    added selected prop to AppRail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,9 @@
 
 <AppShell>
 	<svelte:fragment slot="sidebarLeft">
-		<AppRail>
+		<AppRail selected={storeValue}>
 			<!-- Left Sidebar Content -->
-			<AppRailTile label="Home" value={0}
+			<AppRailTile label="Home" value={0} tag="a" href="/"
 				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
 					><path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z" /></svg
 				></AppRailTile
