5bc979f0320191b43248faab946a95aa8e24b62b
Nick <nconkl1@outlook.com>
2023-05-02 16:11:27 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppRail, AppRailTile, AppShell } from '@skeletonlabs/skeleton';
+	import { writable, type Writable } from 'svelte/store';
+
+	const storeValue: Writable<number> = writable(0);
+</script>
+
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<!-- Left Sidebar Content -->
+			<AppRailTile label="Home" value={0}
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z" /></svg
+				></AppRailTile
+			>
+		</AppRail>
+	</svelte:fragment>
+
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Testing the main content</h1>
+<h2>This is pretty cool</h2>
+<h3>Skeleton is neat</h3>
